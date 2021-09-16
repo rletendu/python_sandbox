5ee04fb78f99b93fc89951436e4939a5cac4ac8f
raphael letendu <raphael.letendu@gmail.com>
2021-09-16 22:23:59 +0200

    wip exa

--- exa.py
+++ exa.py
@@ -5,11 +5,21 @@ import shutil
 import socket
 import threading
 import serial
+import serial.tools.list_ports
+import time
 
 
 LOGGING_FORMAT = '%(asctime)s :: %(levelname)s :: %(name)s :: %(lineno)d :: %(funcName)s :: %(message)s'
 
-
+"""
+"R"
+"EOL"
+"GET_TEMP?"
+"SET_TEMP,85.0,UPPER_BAND,200,LOWER_BAND,200,HOT,SOAK_TIME,3"
+"SET_TEMP,-40.0,UPPER_BAND,200,LOWER_BAND,200,COLD,SOAK_TIME,4"
+"SET_TEMP,25.0,UPPER_BAND,200,LOWER_BAND,200,ROOM,SOAK_TIME,1"
+"TEST_RESULT,1"
+"""
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 DEFAULT_PORT = 65432        # P
@@ -31,13 +41,15 @@ class ExaComTCP(object):
         self.log.info("Received {}".format(data))
         return data
 
+    def __del__(self):
+        self.TcpSoocket.close()
+
 
 class ExaComSerial(object):
     def __init__(self, port) -> None:
         super().__init__()
         self.log = logging.getLogger()
         self.ser = serial.Serial(port, baudrate=9600, timeout=3)
-
 
     def send(self, data):
         self.log.info("Sending {}".format(data))
@@ -48,26 +60,67 @@ class ExaComSerial(object):
         self.log.info("Received {}".format(data))
         return data
 
-
-
 class Exa(object):
-    def __init__(self):
-        self.ExaCom = None
+    def __init__(self, com_port=None, tcp_port=None):
+        if com_port is not None and tcp_port is None:
+            self.ExaCom = ExaComSerial(port=com_port)
+        elif com_port is None and tcp_port is not None:
+            self.ExaCom = ExaComTCP(port=tcp_port)
+        else:
+            raise "Invalid Exatron Interface"
 
     def wait_ready(self):
-        pass
+        r = self.ExaCom.get()
+        if r =="H\r":
+            return True
+        else:
+            return False
 
     def get_temperature(self):
         self.ExaCom.send("GET_TEMP?\r")
-        t = self.ExaCom.get()
-        return ;
+        r = self.ExaCom.get()
+        t = float(r.replace("CUR_TEMP,","").replace("\r"))
+        return t
 
+    def load_next_part(self):
+        self.ExaCom.send("R\r")
+        r = self.ExaCom.get()
+        if r =="OK\r":
+            return True
+        else:
+            return False
+        
+    def unload_part(self, bin=1):
+        self.ExaCom.send("TEST_RESULT,{}\r".format(bin))
+        r = self.ExaCom.get()
+        if r =="OK\r":
+            return True
+        else:
+            return False
 
-
-
+    def set_temperature(self, temp):
+        if temp < 20:
+            shiller = "COLD"
+        elif temp == 25:
+            shiller = "ROOM"
+        elif temp > 25:
+            shiller = "HOT"
+        self.ExaCom.send("SET_TEMP,{},UPPER_BAND,200,LOWER_BAND,200,{},SOAK_TIME,4".format(temp,shiller))
+        r = self.ExaCom.get()
+        if r =="OK\r":
+            return True
+        else:
+            return False
+    
+    def end_of_lot(self):
+        self.ExaCom.send("EOL\r")
+        r = self.ExaCom.get()
+        if r =="OK\r":
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true',
                         help='Activate Debug mode with verbose execution trace information')
@@ -81,5 +134,21 @@ if __name__ == '__main__':
         log.info("Starting")
     else:
         log.setLevel(logging.CRITICAL)
+
+
+	com_list = serial.tools.list_ports.comports()
+    for com in com_list:
+        log.info(com)
+        if args.com is None:
+            args.com = com.device
+            print("No COM port specified using {}".format(args.com))
+            break
+
+    exatron = Exa(com_port=)
+    exatron.wait_ready()
+    exatron.set_temperature(25)
+    exatron.load_next_part()
+    time.sleep(10)
+    exatron.unload_part()
 
     
