68ede4a85b5997247407c4d0b36de4fb124f9243
raphael letendu <raphael.letendu@gmail.com>
2021-09-25 16:43:41 +0200

    Exa gui

---
+++ ExatronARBui2py.bat
@@ -0,0 +1,36 @@
+rem call C:\Python34\Lib\site-packages\PyQt5\pyuic5.bat main_ui.ui -o main_ui.py -x
+python -m PyQt5.uic.pyuic exa_arb_ui.ui -o exa_arb_ui.py -x
+pause
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
